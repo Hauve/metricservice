d0191fae2ef36f90f019eb4491a76974f4ef2fa2
Alexander Demidenko <hurd@yandex-team.ru>
2023-03-28 09:11:41 +0700

    initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/Hauve/metricservice.git/internal/handlers"
-	"github.com/go-chi/chi/v5"
-)
-
-func main() {
-
-	r := chi.NewRouter()
-
-	r.Get("/value/{metricType}/{metricName}", handlers.GetHandler)
-	r.Get("/", handlers.GetAllHandler)
-	r.Post("/update/{metricType}/{metricName}/{metricValue}", handlers.PostHandler)
-
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		panic("Listen and serve failed!")
-	}
-}
+func main() {}
