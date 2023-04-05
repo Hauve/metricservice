97669e3bffc9e3462ae158c4eb5e4dbc23ee9402
Hauve <jemazpn@gmail.com>
2023-04-06 00:22:00 +0300

    changed path processing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -19,7 +19,7 @@ func NewMemStorage() *MemStorage {
 
 var MyMemStorage = NewMemStorage()
 
-func gaugeHandler(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		// разрешаем только POST-запросы
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -27,73 +27,52 @@ func gaugeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	path := r.URL.Path
-	path = strings.TrimPrefix(path, "/update/gauge/")
+	path = strings.TrimPrefix(path, "/update/")
 	pathParts := strings.Split(path, "/")
 	if len(pathParts) != 2 {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	name := pathParts[0]
-	val := pathParts[1]
-	_, err := strconv.ParseInt(val, 10, 64)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	MyMemStorage.data[name] = val
-	w.WriteHeader(http.StatusOK)
+	metric := pathParts[0]
+	name := pathParts[1]
+	val := pathParts[2]
 
-	//_, err = w.Write([]byte("OK"))
-	//if err != nil {
-	//	panic("Недоступна запись в response writer")
-	//}
-}
-
-func counterHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		// разрешаем только POST-запросы
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	path := r.URL.Path
-	path = strings.TrimPrefix(path, "/update/counter/")
-	pathParts := strings.Split(path, "/")
-	if len(pathParts) != 2 {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	name := pathParts[0]
-	val := pathParts[1]
-	floatNewValue, err := strconv.ParseFloat(val, 64)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if _, ok := MyMemStorage.data[name]; !ok {
+	switch metric {
+	case "gauge":
+		_, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 		MyMemStorage.data[name] = val
-	} else {
-		oldValue := MyMemStorage.data[name]
-		floatOldValue, _ := strconv.ParseFloat(oldValue, 64)
-		newValue := fmt.Sprintf("%f", floatNewValue+floatOldValue)
-		MyMemStorage.data[name] = newValue
+	case "counter":
+		floatNewValue, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if _, ok := MyMemStorage.data[name]; !ok {
+			MyMemStorage.data[name] = val
+		} else {
+			oldValue := MyMemStorage.data[name]
+			floatOldValue, _ := strconv.ParseFloat(oldValue, 64)
+			newValue := fmt.Sprintf("%f", floatNewValue+floatOldValue)
+			MyMemStorage.data[name] = newValue
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
-	w.WriteHeader(http.StatusOK)
 
-	//_, err = w.Write([]byte("OK"))
-	//if err != nil {
-	//	panic("Недоступна запись в response writer")
-	//}
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/gauge/", gaugeHandler)
-	mux.HandleFunc("/update/counter/", counterHandler)
+	mux.HandleFunc("/update/", mainHandler)
 	mux.Handle("/", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusNotFound)
+		writer.WriteHeader(http.StatusNotImplemented)
 	}))
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
