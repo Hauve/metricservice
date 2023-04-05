14acc1b2682fd0d4e9df30982853ac0ded4f82a2
Hauve <jemazpn@gmail.com>
2023-04-06 00:41:38 +0300

    fixed unknown operation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,12 +8,14 @@ import (
 )
 
 type MemStorage struct {
-	data map[string]string
+	data      map[string]string
+	myMetrics []string
 }
 
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
-		data: make(map[string]string),
+		data:      make(map[string]string),
+		myMetrics: []string{"gauge", "counter"},
 	}
 }
 
@@ -29,6 +31,18 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	path = strings.TrimPrefix(path, "/update/")
 	pathParts := strings.Split(path, "/")
+
+	ok := false
+	for _, val := range MyMemStorage.myMetrics {
+		if pathParts[0] == val {
+			ok = true
+			break
+		}
+	}
+	if !ok {
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	}
 	if len(pathParts) != 3 {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -39,14 +53,14 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	val := pathParts[2]
 
 	switch metric {
-	case "gauge":
+	case MyMemStorage.myMetrics[0]:
 		_, err := strconv.ParseInt(val, 10, 64)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		MyMemStorage.data[name] = val
-	case "counter":
+	case MyMemStorage.myMetrics[1]:
 		floatNewValue, err := strconv.ParseFloat(val, 64)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -70,7 +84,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", mainHandler)
+	mux.HandleFunc("/update", mainHandler)
 	mux.Handle("/", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusNotImplemented)
 	}))
@@ -79,3 +93,17 @@ func main() {
 		panic("Listen and serve failed!")
 	}
 }
+
+//Error:      	Not equal:
+//expected: 404
+//actual  : 501
+//Messages:   	Несоответствие статус кода ответа ожидаемому в хендлере 'POST http://localhost:8080/updater/counter/testCounter/100'
+//iteration1_test.go:242: Оригинальный запрос:
+//POST /updater/counter/testCounter/100 HTTP/1.1
+
+//Error:      	Not equal:
+//expected: 501
+//actual  : 404
+//Messages:   	Несоответствие статус кода ответа ожидаемому в хендлере 'POST http://localhost:8080/update/unknown/testCounter/100'
+//iteration1_test.go:227: Оригинальный запрос:
+//POST /update/unknown/testCounter/100 HTTP/1.1
