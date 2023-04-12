4d3ac110424da377ff02889962167b1df419e33e
Hauve <jemazpn@gmail.com>
2023-04-13 00:28:27 +0300

    added Storage interface

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/Hauve/metricservice.git/internal/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	service := handlers.New()
+
+	r := chi.NewRouter()
+
+	r.Get("/value/{metricType}/{metricName}", service.GetHandler)
+	r.Get("/", service.GetAllHandler)
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", service.PostHandler)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic("Listen and serve failed!")
+	}
+}
