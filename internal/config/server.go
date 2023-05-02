42f3387e2199963df049f8d5e450c713baeb7ce7
Hauve <jemazpn@gmail.com>
2023-05-02 23:38:40 +0300

    added new params to server config

--- internal/config/server.go
+++ internal/config/server.go
@@ -3,15 +3,45 @@ package config
 import (
 	"flag"
 	"os"
+	"strconv"
+	"time"
 )
 
 type ServerConfig struct {
-	Address string
+	Address         string
+	StoreInterval   time.Duration
+	FileStoragePath string
+	Restore         bool
 }
 
 func LoadServerConfig() *ServerConfig {
 	address := flag.String("a", "localhost:8080", "address")
+
+	storeInterval := flag.Int("i", 300, "store interval")
+	filePath := flag.String("f", "/tmp/metrics-db.json", "file storage path")
+	restore := flag.Bool("r", true, "restore")
 	flag.Parse()
+
+	storeIntervalEnv, ok := os.LookupEnv("STORE_INTERVAL")
+	if ok {
+		temp, err := strconv.Atoi(storeIntervalEnv)
+		if err == nil {
+			*storeInterval = temp
+		}
+	}
+
+	filePathEnv, ok := os.LookupEnv("FILE_STORAGE_PATH")
+	if ok {
+		*filePath = filePathEnv
+	}
+
+	restoreEnv, ok := os.LookupEnv("RESTORE")
+	if ok {
+		temp, err := strconv.ParseBool(restoreEnv)
+		if err == nil {
+			*restore = temp
+		}
+	}
 
 	addrEnv, ok := os.LookupEnv("ADDRESS")
 	if ok {
@@ -19,6 +49,9 @@ func LoadServerConfig() *ServerConfig {
 	}
 
 	return &ServerConfig{
-		Address: *address,
+		Address:         *address,
+		StoreInterval:   time.Duration(*storeInterval) * time.Second,
+		FileStoragePath: *filePath,
+		Restore:         *restore,
 	}
 }
